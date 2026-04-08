e8760a9d9813b2a24f3b611922b10729992d6632
gwamaka1 <mwamtobegwamaka@gmail.com>
2026-04-08 15:36:01 -0400

    finished

--- src/main/java/com/pluarlsight/PayrollCalculato.java
+++ src/main/java/com/pluarlsight/PayrollCalculato.java
@@ -1,4 +1,21 @@
 package com.pluarlsight;
 
+import java.util.Scanner;
+
 public class PayrollCalculato {
+    public static void main(String[] args) {
+        Scanner scanner =  new Scanner(System.in);
+        System.out.println("whats your name?: ");
+        String name = scanner.nextLine();
+        System.out.println("how many hours have you worked?: ");
+        float hoursWorked = scanner.nextFloat();
+        System.out.println("whats your pay rate?: ");
+        float payRate = scanner.nextFloat();
+        float grossPay = hoursWorked * payRate;
+        System.out.printf("%s because you worked %.2f hours and your hourly wage is %.2f your gross pay is  %.2f",name,hoursWorked,payRate,grossPay);
+
+
+
+
+    }
 }
